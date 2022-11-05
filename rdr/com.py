7073a7cbd43fb8dc3a78808aee06685141ff0f63
stephen locas <flansmaster@163.com>
2022-11-05 10:49:27 +0800

    0

--- rdr/com.py
+++ rdr/com.py
@@ -1,12 +1,9 @@
 def combNumber(m, n, b):
-    global totalNumberR
-    totalNumberR = 0
     for i in range(m, n-1, -1):   
         b[n-1] = i
         if n-1>0:
             combNumber(i-1,n-1,b)
         else:
             print (b)
-            totalNumberR += 1
 group=[0,0,0]
 combNumber(5,3,group)
