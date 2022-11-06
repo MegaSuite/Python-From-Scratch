05e6bbf8c64d6455d13c54a630a45790aa9bfa38
stephen locas <flansmaster@163.com>
2022-11-06 15:39:57 +0800

    0

--- rdr/com.py
+++ rdr/com.py
@@ -1,11 +1,7 @@
-def combNumber(m, n, b):
-    for i in range(m, n-1, -1):   
-        b[n-1] = i
-        if n-1>0:
-            combNumber(i-1,n-1,b)
-        else:
-            print (b)
-            
-group=[0,0,0]
-
-combNumber(5,3,group)
+def Combinatorial(n, m):
+    Min = min(m,n-m)
+    result = 1
+    for i in range(0, Min):
+        result = result * (n-i) / (Min-i)
+    return result
+Combinatorial(5,3)
