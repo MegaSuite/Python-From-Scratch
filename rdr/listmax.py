ea51033ae5472bee740d51039255aa06657dc2de
stephen locas <flansmaster@163.com>
2022-11-07 19:28:20 +0800

    deleted

--- rdr/listmax.py
+++
@@ -1,8 +0,0 @@
-nums = [1,6,4,3,5,2]
-def get_max(list):
-    max_value = list[0]
-    for i in range(1, len(list)):
-        if max_value < list[i]:
-            max_value = list[i]
-    print(max_value)
-get_max(nums)
