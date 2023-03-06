f108b858d85fdc29f94ac87bdd72ac614228f441
konrad <flansmaster@163.com>
2023-03-06 20:24:25 +0800

    commit

--- Code Demo/A try.py
+++ Code Demo/A try.py
@@ -7,7 +7,9 @@
 * Date: 2023/02/06 星期一 14:51
 * ===================================================================
 '''
-from sys import argv
-argv=input()
-a,b,c=argv
-print(a,b,c)
+# from sys import argv
+# argv=input()
+# a,b,c=argv
+# print(a,b,c)
+import random
+print(random.randint(1,4))
