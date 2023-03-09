fb0e21e76aa103eadfe83c021b2d775323b30b02
konrad <flansmaster@163.com>
2023-03-09 20:51:59 +0800

    commit

--- function.py
+++
@@ -1,5 +0,0 @@
-def city_func(city,country,population=0):
-    if population:
-        return city.title()+','+country.title()+' - population '+str(population)
-    else:
-        return city.title()+','+country.title()
