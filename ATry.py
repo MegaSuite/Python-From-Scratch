7253a82511ecf691448971b7e19546b3502c9a3a
Konrad Locas <flansmaster@163.com>
2023-07-05 20:14:44 +0800

    modify

--- ATry.py
+++ ATry.py
@@ -1,4 +1,13 @@
-import requests
-import json
-r=requests.get('https://api.eatrice.top/reading')
-print(r.text.Content)
+import re
+num=0
+with open('source.txt','r+') as f:
+    source=f.read()
+
+lista=re.split('[0-9]{1,2}\.',source)
+# print(lista)
+# len(lista)
+with open('out.txt','w+') as f:
+    for item in lista:
+        # item=item.replace('\n','')
+        f.write('```\n'+str(num)+'.'+item+'\n```\n\n')
+        num+=1
