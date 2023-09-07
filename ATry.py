87cdcb41704c3f4ec1f4a56b1d2b6df8081531be
Konrad Locas <flansmaster@163.com>
2023-09-07 19:27:19 +0800

    del Catch_Colonel_Sanders

--- ATry.py
+++ ATry.py
@@ -1,13 +1,10 @@
-import re
-num=0
-with open('source.txt','r+') as f:
-    source=f.read()
-
-lista=re.split('[0-9]{1,2}\.',source)
-# print(lista)
-# len(lista)
-with open('out.txt','w+') as f:
-    for item in lista:
-        # item=item.replace('\n','')
-        f.write('```\n'+str(num)+'.'+item+'\n```\n\n')
-        num+=1
+def count():
+     fs=[]
+     for i in range(1,4):
+             def f():
+                     return i*i
+             fs.append(f)
+     print(fs)
+     return fs
+f1,f2,f3=count()
+print(count())
